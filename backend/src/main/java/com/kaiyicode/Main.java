f1fcce22b8bedfd3276ae863222874f9c5ea2a2d
inthesum <50326416+inthesum@users.noreply.github.com>
2024-12-29 11:30:46 +0100

    added a new line

--- backend/src/main/java/com/kaiyicode/Main.java
+++ backend/src/main/java/com/kaiyicode/Main.java
@@ -14,6 +14,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+        System.out.println("Application started");
     }
 
     @Bean
