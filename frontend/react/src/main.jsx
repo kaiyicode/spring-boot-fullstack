ac31fb7c1d8b79db2f5db2cdc301e7badacfca68
inthesum <50326416+inthesum@users.noreply.github.com>
2025-02-23 16:54:20 +0100

    Change link color to blue

--- frontend/react/src/main.jsx
+++ frontend/react/src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from "@chakra-ui/react"
 import './index.css'
 import App from './App.jsx'
+import { createStandaloneToast } from '@chakra-ui/react'
+
+const { ToastContainer, toast } = createStandaloneToast()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <ChakraProvider>
           <App />
+          <ToastContainer/>
       </ChakraProvider>
   </StrictMode>,
 )
--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -1,7 +1,14 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      'localhost',
+      'kaiyicode-api-v3-env.eba-dedtjmmh.eu-north-1.elasticbeanstalk.com',
+      // Add other allowed hosts here if needed
+    ],
+  },
+});
